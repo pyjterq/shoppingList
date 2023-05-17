548cc2bbc59b1ae4a429678cbedc8c9197dafc77
pyjter <pyjter@pm.me>
2023-05-17 09:17:32 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+Quick start:
+
+```
+$ npm install
+$ npm start
+```
+
+Head over to https://vitejs.dev/ to learn more about using vite
+
---
+++ index.js
@@ -0,0 +1,75 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js"
+import { getDatabase, ref, push, onValue, remove } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js"
+
+const appSettings = {
+    databaseURL: "https://realtime-database-cbef4-default-rtdb.europe-west1.firebasedatabase.app/"
+}
+
+const app = initializeApp(appSettings)
+const database = getDatabase(app)
+const shoppingListInDB = ref(database, "shoppingList")
+
+const inputFieldEl = document.getElementById("input-field")
+const addButtonEl = document.getElementById("add-button")
+const shoppingListEl = document.getElementById("shopping-list")
+
+addButtonEl.addEventListener("click", function() {
+    let inputValue = inputFieldEl.value
+    if (inputValue === ""){
+      return 
+  } else {
+      push(shoppingListInDB, inputValue)
+      
+      clearInputFieldEl()
+   }
+})
+inputFieldEl.addEventListener("keydown", function(event) {
+    let inputValue = inputFieldEl.value
+    if (event.key === "Enter" && inputValue !== "") {
+        addButtonEl.click()
+        clearInputFieldEl()
+    } else if (event.key === "Enter") {
+      return 
+  }
+}
+)
+
+onValue(shoppingListInDB, function(snapshot) {
+    if (snapshot.exists()) {
+        let itemsArray = Object.entries(snapshot.val())
+    
+        clearShoppingListEl()
+        
+        for (let i = 0; i < itemsArray.length; i++) {
+            let currentItem = itemsArray[i]
+            appendItemToShoppingListEl(currentItem)
+        }    
+    } else {
+        shoppingListEl.innerHTML = '<p class="message">No items here... yet</p>'
+    }
+})
+
+function clearShoppingListEl() {
+    shoppingListEl.innerHTML = ""
+}
+
+function clearInputFieldEl() {
+    inputFieldEl.value = ""
+}
+
+function appendItemToShoppingListEl(item) {
+    let itemID = item[0]
+    let itemValue = item[1]
+    
+    let newEl = document.createElement("li")
+    
+    newEl.textContent = itemValue
+    
+    newEl.addEventListener("click", function() {
+        let exactLocationOfItemInDB = ref(database, `shoppingList/${itemID}`)
+        
+        remove(exactLocationOfItemInDB)
+    })
+    
+    shoppingListEl.append(newEl)
+}
